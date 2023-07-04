788f832653f0942f0763f2c66a1e465651abf71d
qwert0319 <mpo0815@naver.com>
2023-07-04 13:33:59 +0900

    feat:배포

---
+++ src/test/java/org/edupoll/AppMoimApplicationTests.java
@@ -0,0 +1,33 @@
+package org.edupoll;
+
+import java.util.List;
+
+import org.edupoll.model.entity.Moim;
+import org.edupoll.model.entity.User;
+import org.edupoll.repository.MoimRepository;
+import org.edupoll.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMoimApplicationTests {
+	
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	MoimRepository moimRepository;
+	
+	@Test
+	void contextLoads() {
+		User found = userRepository.findById("zoro77").get();
+		System.out.println(found.getId());
+		List<Moim> attends = found.getAttendMoims();
+		for(Moim m : attends) {
+			System.out.println(m.getId() +" / "+ m.getTitle());
+		}
+		
+	}
+	
+
+}
